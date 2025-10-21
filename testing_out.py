2fa878f94cb8cc04be4288e26115a2b340c0ceac
sofiesana <119871294+sofiesana@users.noreply.github.com>
2025-10-21 12:55:04 +0200

    cleaning

--- testing_out.py
+++ testing_out.py
@@ -40,9 +40,6 @@ def run_episode(env, agent, mode, buffer: Buffer):
 
     hidden_states = np.zeros((n_agents, buffer.hidden_state_dim))  # example initial hidden state 
     while not episode_ended:
-        # env.render()
-        # pause until key press
-        # input("Press Enter to continue...")
 
         action, log_probs, hidden_states = agent.choose_action(observation, hidden_states)
         # action = env.action_space.sample()  # Random action for placeholder
@@ -58,36 +55,7 @@ def run_episode(env, agent, mode, buffer: Buffer):
             dones=terminated,
             hidden_states=hidden_states,
             log_probs=log_probs
-        )
-        # buffer.print_attributes()
-        # buffer.print_buffer()
-        # batch = buffer.sample_agent_batch(agent_index=0, batch_size=10, window_size=5)
-
-        # you can also store single agent transition if needed
-        # dummy_single_agent_transition = {
-        #     "global_state": global_state,
-        #     "observation": observation[0],
-        #     "action": action[0],
-        #     "reward": reward[0],
-        #     "next_observation": new_observation[0],
-        #     "done": terminated,
-        #     "hidden_state": dummy_hidden_state[0]
-        # }
-
-        # buffer.store_single_agent_transition(
-        #     agent_index=0,
-        #     global_state=dummy_single_agent_transition["global_state"],
-        #     observation=dummy_single_agent_transition["observation"],
-        #     action=dummy_single_agent_transition["action"],
-        #     reward=dummy_single_agent_transition["reward"],
-        #     next_observation=dummy_single_agent_transition["next_observation"],
-        #     done=dummy_single_agent_transition["done"],
-        #     hidden_state=dummy_single_agent_transition["hidden_state"]
-        # )
-
-        # to check if batching is working:
-        # print("actions of Sampled batch for agent 0:", batch[5] if batch is not None else "No batch sampled")
-        # print("Sampled batch for agent 0:", batch if batch is not None else "No batch sampled")
+        )       
 
         ep_return.append(reward)
 
@@ -128,18 +96,17 @@ def run_episodes(env, agent, num_episodes, plotter, mode='train'):
         print(f"Episode {ep} | mean return: {reward_sum} | terminated: {bool(terminated)}")
 
     if mode == 'train':
-        # agent.save_rewards()
-        # agent.save_model()
+        
         for epoch in range(N_TRAIN_EPOCHS):
             print(f"Training epoch {epoch + 1}/{N_TRAIN_EPOCHS}")
             actor_loss_list, critic_loss = agent.learn(buffer)
-            # print("Actor 1 loss:", actor_loss_list[0], " ---  Actor 2 loss:", actor_loss_list[1], " ---  Critic loss:", critic_loss)
+
             plotter.update(np.mean(actor_loss_list))
 
         agent.update_prev_actor_models() # Update prev network to current before optimizing current
     elif mode == 'test':
         print(f"Average test return: {np.mean(agent.test_returns)}")
-        # agent.save_test_returns()
+
 
     return returns, actor_loss_list, critic_loss
 
@@ -151,22 +118,14 @@ def run_environment(args):
     plotter = LiveLossPlotter()
     for iteration in range(ITERS):
         print(f"Iteration {iteration + 1}/{ITERS}")
-        # add iteration to args
+        
         env = gym.make("rware-tiny-2ag-v2")
         agent = agent_factory.create_agent(agent_type="MAPPOSE", env=env, batch_size=16)
-        # agent = 0
-        # make_data_folder(agent.path)
-        
-        # Training phase
-        # print(f"Training {agent.agentName} agent...")
+    
         returns, actor_loss_list, critic_loss = run_episodes(env, agent, N_TRAIN_EPISODES, plotter, mode='train')
-        
-        # Testing phase
-        # print(f"Testing {agent.agentName} agent...")
-        # run_episodes(env, agent, N_TEST_EPISODES, mode='test')
+    
     env.close()
 
 
 if __name__ == "__main__":
     run_environment(None)
-    # inspect_environment(gym.make("rware-tiny-2ag-v2"))
